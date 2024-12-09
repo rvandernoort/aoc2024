9aae04f27a5e6b5c3b61eaf7766e252464b18c27
Rover van der Noort <rovervandernoort@protonmail.com>
2024-12-09 19:23:38 +0100

    add day5 part2

--- day5/main.go
+++ day5/main.go
@@ -9,17 +9,7 @@ import (
 
 func Part1(ordering map[uint64][]uint64, updates [][]uint64) (output uint64) {
 	for _, row := range updates {
-		correct := true
-		for i := 0; i < len(row); i++ {
-			orders := ordering[row[i]]
-			for _, order := range orders {
-				for j := 0; j < i; j++ {
-					if row[j] == order {
-						correct = false
-					}
-				}
-			}
-		}
+		correct := checkOrdering(ordering, row)
 		if correct {
 			middle := len(row) / 2
 			output += row[middle]
@@ -43,38 +33,39 @@ func checkOrdering(ordering map[uint64][]uint64, row []uint64) (correct bool) {
 	return
 }
 
-func Part2(ordering map[uint64][]uint64, updates [][]uint64) (output uint64) {
-	for _, row := range updates {
-		correct := true
-		for i := 0; i < len(row); i++ {
-			orders := ordering[row[i]]
-			for _, order := range orders {
-				for j := 0; j < i; j++ {
-					if row[j] == order {
-						correct = false
+func checkLine(row []uint64, output uint64, ordering map[uint64][]uint64) uint64 {
+	for i := 0; i < len(row); i++ {
+		item := row[i]
+		orders := ordering[item]
+		for _, order := range orders {
+			for j := 0; j < i; j++ {
+				if row[j] == order {
+					newRow := make([]uint64, len(row))
+					copy(newRow, row)
+					safe := newRow[i]
+					newRow = framework.Remove(newRow, i)
+					newRow = append(newRow[:i-1], append([]uint64{safe}, newRow[i-1:]...)...)
+
+					if checkOrdering(ordering, newRow) {
+						middle := len(newRow) / 2
+						output += newRow[middle]
+						return output
+					} else {
+						output += checkLine(newRow, output, ordering)
+						return output
 					}
 				}
 			}
 		}
+	}
+	return output
+}
+
+func Part2(ordering map[uint64][]uint64, updates [][]uint64) (output uint64) {
+	for _, row := range updates {
+		correct := checkOrdering(ordering, row)
 		if !correct {
-			for i := 0; i < len(row); i++ {
-				orders := ordering[row[i]]
-				for _, order := range orders {
-					for j := 0; j < i; j++ {
-						if row[j] == order {
-							newRow := make([]uint64, len(row))
-							copy(newRow, row)
-							newRow = framework.Remove(newRow, i)
-							newRow = append(newRow[:j+1], append([]uint64{row[i]}, newRow[j+1:]...)...)
-							if checkOrdering(ordering, newRow) {
-								middle := len(newRow) / 2
-								output += newRow[middle]
-								return
-							}
-						}
-					}
-				}
-			}
+			output += checkLine(row, 0, ordering)
 		}
 	}
 	return
--- day5/main_test.go
+++ day5/main_test.go
@@ -18,7 +18,7 @@ func TestPart1(t *testing.T) {
 
 func TestPart2(t *testing.T) {
 	input := framework.ReadData("test_input.txt")
-	want := uint64(241861950)
+	want := uint64(123)
 	ordering, updates := framework.ParsePages(input)
 	got := Part2(ordering, updates)
 	if got != want {
